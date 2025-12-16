67f1d4eb2847a296581ddccffbbb163d534fa486
Juan <jazavalagaf@gmail.com>
2025-12-15 22:53:39 -0500

    updating comments message

--- LTI_config.py
+++ LTI_config.py
@@ -16,5 +16,24 @@ CONFIGURACION_COMUN = {
 
 LTI_CONFIG = {
     "https://lti-ri.imsglobal.org": CONFIGURACION_COMUN,
-    "flask1234": CONFIGURACION_COMUN
+    "flask1234": CONFIGURACION_COMUN,
+    "https://sandbox.moodledemo.net": {
+        
+        # 1. THIS IS THE CLIENT ID THAT THE LMS (MOODLE) GAVE YOU WHEN YOU REGISTERED YOUR TOOL (talk to LMS admin):
+        "client_id": "pHtfspCBFqL4HIb",
+        
+        # 2. STATICS URLs OF MOODLE (You need to talk to LMS admin to get this enpoints):
+        "auth_login_url": "https://sandbox.moodledemo.net/mod/lti/auth.php",
+        "auth_token_url": "https://sandbox.moodledemo.net/mod/lti/token.php",
+        "key_set_url": "https://sandbox.moodledemo.net/mod/lti/certs.php",
+        
+        # 3. Here goes your keys (the name of the files):
+        "private_key_file": "private.key",
+        "public_key_file": "public.key",
+        
+        # 4. DEPLOYMENT ID:
+        # In Moodle, the Deployment ID normally is "2" for manual tools (sandbox). 
+        # to be sure, we can put many ids:
+        "deployment_ids": ["1", "2", "3", "4", "5"] 
+    }
 }
--- main.py
+++ main.py
@@ -13,9 +13,14 @@ from LTI_config import LTI_CONFIG
 
 app = Flask(__name__)
 load_dotenv()
+
+# This proxy fix is needed when working behind a proxy (like in production with nginx, or our case, in our testing using ngrok)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
+# Digital signature secret key for sessions (you need to use a long a complex string here, but save it in .env)
 app.secret_key = os.urandom(24)
+
+# Session cookie settings (important to work and maintain sessions in iframes)
 app.config['SESSION_COOKIE_NAME'] = 'lti_connector_session'
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'
 app.config['SESSION_COOKIE_SECURE'] = True
@@ -27,8 +32,10 @@ def get_tool_conf():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     try:
+        # This function retrieves the tool configuration (LTI_config)
         tool_conf = get_tool_conf()
 
+        # Here the server catches the cookies and session info from the incoming request
         flask_request = FlaskRequest(
             cookies=request.cookies,
             session=session,
@@ -37,36 +44,36 @@ def login():
 
         oidc_login = FlaskOIDCLogin(flask_request, tool_conf)
 
+        # Request host is just host:port of the actual server
         launch_url = f"https://{request.host}/launch"
+
+        # Here internally creates the login redirect response (mentioned in documentation)
         return oidc_login.redirect(launch_url)
+    
     except Exception as e:
-        #print(f"Error en Login: {e}")
         return f"Error in Login Init: {e}", 500
     
 @app.route("/launch", methods=['GET', 'POST'])
 def launch():
-    # --- DEBUGGING (cookies) ---
-    print(f"- Cookies recibidas: {request.cookies}")
-    print(f"- Form Data (state): {request.form.get('state')}")
-    # -------------------------------------------------------
-    state_param = request.form.get('state')
-    print(f"- Form Data (state): {state_param}")
-
     try:
         tool_conf = get_tool_conf()
         flask_request = FlaskRequest()
 
+        # Create the message launch object
         message_launch = FlaskMessageLaunch(flask_request, tool_conf)
 
+        # Extract launch data (decode and verify the JWT sent by the LMS)
         launch_data = message_launch.get_launch_data()
-        #print("Launch validado correctamente")
 
         #Extract decode user (client) data
-        email = launch_data.get('email')
-        name = launch_data.get('name')
-        sub = launch_data.get('sub') # ID único del usuario en Blackboard
+        email = launch_data.get('email') # Student email
+        name = launch_data.get('name') # Student full name
+        sub = launch_data.get('sub') # Unique ID in the LMS (ex. unique id inside Blackboard)
         roles = launch_data.get('https://purl.imsglobal.org/spec/lti/claim/roles', [])
         context = launch_data.get('https://purl.imsglobal.org/spec/lti/claim/context', {})
+
+        #HERE GOES THE USERS DB MATCH LOGIC (retrieve user info from your DB if user exists, if not, create it) -> integration with SABIO's DBs and backend
+        #Then create the JWT with the same secret as the main backend server
 
         payload = {
             "sub": sub,
@@ -77,15 +84,17 @@ def launch():
             "course_title": context.get('title'),
             "source": "lti",
             "iat": time.time(),
-            "exp": time.time() + 60 # Expires in 60 seconds (muy corto, por seguridad)
+            "exp": time.time() + 60 # Expires in 60 seconds (too short, for security reasons -just for example-)
         }
         
+        # Create the JWT to handoff to the main application
         handoff_token = jwt.encode(payload, os.getenv("SHARED_SECRET"), algorithm=os.getenv("ALGORITHM"))
         
+        # Redirect to the home page (SABIO's front-end) with the token as URL parameter
+        # Then the frontend will use this token to authenticate with the main backend
         return redirect(f"{os.getenv('MAIN_APP_URL')}/home?token={handoff_token}")
     
     except Exception as e:
-        #print(f"Error en Launch: {e}")
         return f"Error en LTI Launch: {e} <br><br> (Watch out the cookies sent)", 401
 
 @app.route('/jwks', methods=['GET'])
